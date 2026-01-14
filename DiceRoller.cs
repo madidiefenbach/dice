497da63335e715dc6d63b54d0b1150fe478f7b8d
madidiefenbach <madidiefenbach@gmail.com>
2026-01-14 14:55:45 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    public class DiceRoller
+    {
+        private readonly Random _random = new Random();
+
+        public int[] RollDice(int rolls)
+        {
+            int[] results = new int[13];
+
+            for (int i = 0; i < rolls; i++)
+            {
+                int die1 = _random.Next(1, 7);
+                int die2 = _random.Next(1, 7);
+                int sum = die1 + die2;
+
+                results[sum]++; // in posiion of the numbner rolled add one to that position in the array
+            }
+            return results;
+        }
+
+    }
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Madison Diefenbach
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+
+        int rolls = int.Parse(Console.ReadLine());
+
+        DiceRoller roller = new DiceRoller();
+        int[] results = roller.RollDice(rolls);
+
+        Console.WriteLine();
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {rolls}. ");
+        Console.WriteLine();
+
+        for (int sum = 2; sum<=12; sum++) // loops through each possible number
+        {
+            int count = results[sum];
+            double percentage = (count / (double)rolls) * 100;
+
+            int stars = (int)Math.Round(percentage, MidpointRounding.AwayFromZero);
+
+            Console.Write($"{sum}: ");
+            Console.WriteLine(new string('*', stars));
+        }
+        Console.WriteLine();
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
